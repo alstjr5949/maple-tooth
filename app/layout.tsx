04554e50815b336cecf5ff1416dddc2176ae2eb0
alstjr5949 <alstjr5949@gmail.com>
2025-03-05 22:41:25 +0900

    feat: home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Image from "next/image";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -15,6 +16,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "메이플 랜드 어금니 계산기",
   description: "메이플 랜드 어금니 계산기",
+  icons: {
+    icon: "/zombie-teeth.png",
+  },
 };
 
 export default function RootLayout({
@@ -27,6 +31,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <header className="flex justify-center items-center py-4 border-b-1 border-gray-200">
+          <Image src="/zombie-teeth.png" alt="logo" width={50} height={50} />
+        </header>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,41 @@
+import Image from "next/image";
+
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div className="flex flex-col items-center justify-center gap-8 h-screen">
+      <h1 className="flex justify-center items-center">
+        <Image src="/zombie.webp" alt="logo" width={70} height={70} />
+      </h1>
+      <div className="flex items-center gap-4">
+        <form className="flex flex-col justify-center gap-2" action="">
+          <label htmlFor="start-level">시작 레벨</label>
+          <input
+            id="start-level"
+            className="border-1 border-gray-200 rounded-md p-2"
+            type="number"
+            name="start-level"
+            min={11}
+          />
+          <label htmlFor="end-level">종료 레벨</label>
+          <input
+            id="end-level"
+            className="border-1 border-gray-200 rounded-md p-2"
+            type="number"
+            name="end-level"
+            max={200}
+          />
+          <button
+            className="bg-blue-500 text-white p-2 rounded-md"
+            type="submit"
+          >
+            계산
+          </button>
+        </form>
+        <div className="flex flex-col items-center gap-2">
+          <p className="text-lg">총 필요한 어금니 개수</p>
+          <p className="text-6xl font-black">100000</p>
+        </div>
+      </div>
+    </div>
+  );
 }
